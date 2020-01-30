9e4947794c9c5537cb3beb5584ea95f477ec9029
King <36568008@qq.com>
2020-01-30 17:59:52 +0800

    no commit message

---
+++ cloudfunctions-aliyun/search/index.js
@@ -0,0 +1,19 @@
+'use strict';
+const db = uniCloud.database()
+exports.main = async (event, context) => {
+	const collection = db.collection('member')
+	if (event.searchKey != '') {
+		const dbCmd = db.command;
+		const res = await collection.where(dbCmd.or({
+			name: new RegExp(event.searchKey)
+		}, {
+			address: new RegExp(event.searchKey)
+		})).orderBy("regtime", "desc").skip((event.page - 1) * event.pageSize).limit(event.pageSize).get();
+		return res
+	} else {
+		const res = await collection.orderBy("regtime", "desc").skip((event.page - 1) * event.pageSize).limit(event.pageSize)
+			.get();
+		return res
+	}
+
+};
