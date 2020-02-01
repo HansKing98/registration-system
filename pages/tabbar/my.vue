73ec914a21afd8fe2ab30aba34529bd3faa4d219
十年的鱼 <31853722@qq.com>
2020-02-01 21:07:47 +0800

    update pages/tabbar/my.vue.

--- pages/tabbar/my.vue
+++ pages/tabbar/my.vue
@@ -1,4 +1,4 @@
-//我在做这个文件，我的群昵称是AHello 
+//我在做这个文件，我的群昵称是AHello <!--丛林野战军2020.2.1修改，增加二维码-->
 <template>
 	<view>		
 		<uni-list>			
@@ -7,6 +7,7 @@
 			<uni-list-item title="关于" thumb="/static/font/about.png" @click="aboutUs()"></uni-list-item>
 			<uni-list-item title="切换账号/退出" thumb="/static/font/exit.png" @click="exit()"></uni-list-item>
 		</uni-list>
+		<view class="list"><x style="">扫码直接填写信息</x><x style=""><image :src="ewm" style="width:300upx;height:300upx;margin:auto;"></image></x></view>
 	</view>
 	
 </template>
@@ -15,14 +16,23 @@
 	import uniList from "@/components/uni-list/uni-list.vue"
 	import uniListItem from "@/components/uni-list-item/uni-list-item.vue"
 	import uniIcon from "@/components/uni-icons/uni-icons.vue"
+	import QR from '../../common/qrcode.js';
 	export default {	
 		components: {uniList,uniListItem,uniIcon},
 		data() {
 			return {				
 				username:'张三',
-				phone:"15022103112"
+				phone:"15022103112",
+				ewm:''  //二维码
 			}
 		},
+		onLoad() {
+			let userid="user"
+			let ewmurl="http://www.anjnet.com/xinguan/#/pages/index/add?id="+userid;
+			this.ewm=QR.createQrCodeImg(ewmurl, {
+										size: 350
+									});
+		}
 		onShow() {
 			//TODO 此处需要根据登录设置适当调整
 			let that = this ;
@@ -63,5 +73,12 @@
 </script>
 
 <style>
-
+	.list{
+		height:350upx;
+		display: flex;
+		flex-direction: column;
+		margin:20upx;
+        background:#f0f0f0;
+       margin:auto;
+	}
 </style>
